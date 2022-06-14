859b06f2af9ec36e43a118b5db15d5f75d15abee
Martina La Rosa <larosamf@gmail.com>
2022-06-13 21:11:23 -0300

    Agrego solicitudes

--- api/src/main/java/com/rescatapp/api/domain/SolicitudDeTransito.java
+++ api/src/main/java/com/rescatapp/api/domain/SolicitudDeTransito.java
@@ -2,50 +2,8 @@ package com.rescatapp.api.domain;
 
 import java.util.Date;
 
-public class SolicitudDeTransito {
-    private Long id;
-    private Date fechaDeCreación;
-    private Date fechaDeRespuesta;
-    private boolean estaAprobada;
-    private Mascota mascota;
-    private Long idSolicitante;
-    private Long idSolicitado;
-    private boolean enCurso;
-
+public class SolicitudDeTransito extends SolicitudCambioDeResposableDeMascota {
     public SolicitudDeTransito(Long id, Date fechaDeCreación, Mascota mascota, Long idSolicitante, Long idSolicitado) {
-        this.id = id;
-        this.fechaDeCreación = fechaDeCreación;
-        this.fechaDeRespuesta = null;
-        this.estaAprobada = false;
-        this.mascota = mascota;
-        this.idSolicitante = idSolicitante;
-        this.idSolicitado = idSolicitado;
-        this.enCurso = true;
-    }
-
-    public boolean aprobar() {
-        this.estaAprobada = true;
-        this.enCurso = false;
-        this.fechaDeRespuesta = new Date();
-        return true;
-    }
-
-    public boolean rechazar() {
-        this.estaAprobada = false;
-        this.enCurso = false;
-        this.fechaDeRespuesta = new Date();
-        return false;
-    }
-
-    public Mascota getMascota() {
-        return this.mascota;
-    }
-
-    public boolean tieneEstadoAprobada() {
-        return this.estaAprobada;
-    }
-
-    public boolean estaEnCurso() {
-        return this.enCurso;
+        super(id, fechaDeCreación, mascota, idSolicitante, idSolicitado);
     }
 }
