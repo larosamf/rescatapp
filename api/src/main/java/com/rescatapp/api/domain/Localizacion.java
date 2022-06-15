3c6716a3653d9a322cfc6052fab0e8826d3d2e88
Martina La Rosa <larosamf@gmail.com>
2022-06-14 21:43:25 -0300

    Agrego diferencia entre localizaciones

--- api/src/main/java/com/rescatapp/api/domain/Localizacion.java
+++ api/src/main/java/com/rescatapp/api/domain/Localizacion.java
@@ -12,8 +12,19 @@ public class Localizacion {
         this.direccion = direccion;
     }
 
-    public Float calcularDistanciaEnMetros(Localizacion otraLocalizacion) {
-        return 0F;
+    public double calcularDistanciaEnKilometros(Localizacion otraLocalizacion) {
+        double radioTierra = 6371;//en kilómetros
+
+        double dLat = Math.toRadians(otraLocalizacion.getLatitud() - this.latitud);
+        double dLng = Math.toRadians(otraLocalizacion.getLongitud() - this.longitud);
+        double sindLat = Math.sin(dLat / 2);
+        double sindLng = Math.sin(dLng / 2);
+        double va1 = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+                * Math.cos(Math.toRadians(this.latitud)) * Math.cos(Math.toRadians(otraLocalizacion.getLatitud()));
+        double va2 = 2 * Math.atan2(Math.sqrt(va1), Math.sqrt(1 - va1));
+        double distancia = radioTierra * va2;
+
+        return distancia;
     }
 
     public Float getLatitud() {
