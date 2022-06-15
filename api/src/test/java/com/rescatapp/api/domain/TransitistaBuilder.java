4059a92ab63027c5fda17ee178803c074bef1a13
Martina La Rosa <larosamf@gmail.com>
2022-06-14 21:52:50 -0300

    Agrego build de Transitista para test

--- api/src/test/java/com/rescatapp/api/domain/TransitistaBuilder.java
+++ api/src/test/java/com/rescatapp/api/domain/TransitistaBuilder.java
@@ -1,6 +1,9 @@
 package com.rescatapp.api.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class TransitistaBuilder {
     private Transitista transitista;
@@ -13,6 +16,15 @@ public class TransitistaBuilder {
         TransitistaBuilder transitistaBuilder = new TransitistaBuilder();
         transitistaBuilder.transitista = new Transitista(1L, new Localizacion(-50f, -50f, "prueba"), "prueba", "1234", "test@†est.com",3,null);
         transitistaBuilder.transitista.solicitudesDeTransito = solicitudes;
+        return transitistaBuilder;
+    }
+
+    public static TransitistaBuilder transitistaActivoConMascotas(){
+        TransitistaBuilder transitistaBuilder = new TransitistaBuilder();
+        transitistaBuilder.transitista = new Transitista(1L, new Localizacion(-50f, -50f, "prueba"), "prueba", "1234", "test@†est.com",3,null);
+        List<Mascota> mascotas = asList(new Mascota(8L, Mascota.Tipo.PERRO), new Mascota(9L, Mascota.Tipo.PERRO), new Mascota(10L, Mascota.Tipo.PERRO));
+        transitistaBuilder.transitista.mascotasTransitadoActualmente = mascotas;
+        transitistaBuilder.transitista.mostrarComoActivo();
         return transitistaBuilder;
     }
 
