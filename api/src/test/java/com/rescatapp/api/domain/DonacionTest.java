cd801f1a20970373cc673237a92b94540f1b6edd
Tyavicoli <tyavicoli@fi.uba.ar>
2022-07-17 14:20:11 -0300

    Merge

--- api/src/test/java/com/rescatapp/api/domain/DonacionTest.java
+++ api/src/test/java/com/rescatapp/api/domain/DonacionTest.java
@@ -13,14 +13,15 @@ public class DonacionTest {
 
     @Test
     public void getMontoACobrar() {
-        Donacion donacion = new Donacion(1L, new BigDecimal("100"), new BigDecimal(1));
+        Donacion donacion = new Donacion(1L, new BigDecimal("100"));
 
         assertThat(donacion.getMontoACobrar()).isEqualByComparingTo(new BigDecimal(100));
     }
 
     @Test
-    public void getMontoADepositar() {
-        Donacion donacion = new Donacion(1L, new BigDecimal(100), new BigDecimal(1));
+    public void procesarConCantidadDeDonacionesMayorAlLimiteBajaComision() {
+        Donacion donacion = new Donacion(1L, new BigDecimal("100"));
+        ProcesadorPagos procesadorPagos = mock(ProcesadorPagos.class);
 
         String cbu = "1111111111";
         donacion.procesar(cbu, 10, procesadorPagos);
@@ -37,8 +38,37 @@ public class DonacionTest {
     }
 
     @Test
-    public void getMontoComision() {
-        Donacion donacion = new Donacion(1L, new BigDecimal(100), new BigDecimal(1));
+    public void procesarConCantidadDeDonacionesMenorAlLimiteCobraComisionRegular() {
+        Donacion donacion = new Donacion(1L, new BigDecimal("100"));
+        ProcesadorPagos procesadorPagos = mock(ProcesadorPagos.class);
+
+        String cbu = "1111111111";
+        donacion.procesar(cbu, 1, procesadorPagos);
+        ArgumentCaptor<BigDecimal> montoApagar = ArgumentCaptor.forClass(BigDecimal.class);
+        ArgumentCaptor<String> cbuEnviado = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<BigDecimal> comisionRecaudada = ArgumentCaptor.forClass(BigDecimal.class);
+
+        verify(procesadorPagos).pagar(montoApagar.capture(), cbuEnviado.capture());
+        verify(procesadorPagos).recaudar(comisionRecaudada.capture());
+
+        assertThat(montoApagar.getValue()).isEqualByComparingTo(new BigDecimal(90));
+        assertThat(comisionRecaudada.getValue()).isEqualByComparingTo(new BigDecimal(10));
+        assertThat(cbuEnviado.getValue()).isEqualTo(cbu);
+    }
+
+    @Test
+    public void procesarConComisionMayorAlLimiteUsaLimiteDeComision() {
+        Donacion donacion = new Donacion(1L, new BigDecimal("50000"));
+        ProcesadorPagos procesadorPagos = mock(ProcesadorPagos.class);
+
+        String cbu = "1111111111";
+        donacion.procesar(cbu, 10, procesadorPagos);
+        ArgumentCaptor<BigDecimal> montoApagar = ArgumentCaptor.forClass(BigDecimal.class);
+        ArgumentCaptor<String> cbuEnviado = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<BigDecimal> comisionRecaudada = ArgumentCaptor.forClass(BigDecimal.class);
+
+        verify(procesadorPagos).pagar(montoApagar.capture(), cbuEnviado.capture());
+        verify(procesadorPagos).recaudar(comisionRecaudada.capture());
 
         assertThat(montoApagar.getValue()).isEqualByComparingTo(new BigDecimal(49000));
         assertThat(comisionRecaudada.getValue()).isEqualByComparingTo(new BigDecimal(1000));
